f97d0cff080d29fbf350d47f0594df882f426673
Jose Castaneda <castaneda@pythian.com>
2016-12-06 00:34:18 +0000

    Setting up issues, file permission and sshd configs

--- init.pp
+++ init.pp
@@ -24,6 +24,15 @@ class { '::login_defs':
 
 }
 
+#Setting access only to root to these binaries
+$rootbins = [ '/usr/bin/wget', '/usr/bin/gcc', '/usr/bin/nc', ]
+file { $rootbins:
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0700',
+}
+
+
 #ucredit=-1, the number of capital characters required in password
 #lcredit=-1, the number of lower case characters required in password
 #dcredit=-1, the number numbers required in password
@@ -36,6 +45,7 @@ pam { "Set cracklib limits in password-auth":
   module    => 'pam_cracklib.so',
   arguments => ['try_first_pass','retry=3', 'minlen=10','ucredit=-1','lcredit=-1','dcredit=-1','ocredit=-1'],
 }
+
 
 pam { "Set password remember limit in password-auth":
   ensure    => present,
@@ -62,5 +72,30 @@ file { '/etc/security/opasswd':
   mode   => '0600',
 }
 
+class { 'ssh::server':
+      storeconfigs_enabled => false,
+      options => {
+        #'PasswordAuthentication' => 'no',
+        #'PermitRootLogin'        => 'no',
+        #'Port'                   => [22, 2222],
+        'Banner'	          => '/etc/issue.net',
+      },
+}
 
 
+class ssh {
+service { 'sshd':
+    ensure  => 'running',
+    enable  => true,
+    require => Package['openssh-server'],
+  }
+
+# add a notify to the file resource
+  file { '/etc/ssh/sshd_config':
+    notify  => Service['sshd'],  # this sets up the relationship
+    mode    => '0600',
+    owner   => 'root',
+    group   => 'root',
+  }
+
+}
