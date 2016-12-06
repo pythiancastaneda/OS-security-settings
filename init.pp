2425bd18dc64680eefe5f1ecde7fe0f0e484d0dc
Jose Castaneda <castaneda@pythian.com>
2016-12-06 21:13:00 +0000

    SSHd configurations, files backups and resources relations

--- init.pp
+++ init.pp
@@ -1,3 +1,8 @@
+
+# Setting up issue file
+exec { "cp /etc/issue /root;":
+  path   => '/usr/bin:/usr/sbin:/bin',
+}~>
 file { '/etc/issue':
   ensure => file,
   owner  => 'root',
@@ -6,6 +11,10 @@ file { '/etc/issue':
   source => 'puppet:///modules/custom_settings/issue',
 }
 
+# Setting up issue.net file for remote logins
+exec { "cp /etc/issue.net /root;":
+  path   => '/usr/bin:/usr/sbin:/bin',
+}~>
 file { '/etc/issue.net':
   ensure => file,
   owner  => 'root',
@@ -14,6 +23,10 @@ file { '/etc/issue.net':
   source => 'puppet:///modules/custom_settings/issue',
 }
 
+# Setting up Password age settings for new accounts 
+exec { "cp /etc/login.defs /root;":
+  path   => '/usr/bin:/usr/sbin:/bin',
+}~>
 class { '::login_defs':
   options => {
     'PASS_MAX_DAYS'   => '99999', # Recommended 90 
@@ -32,29 +45,28 @@ file { $rootbins:
   mode   => '0700',
 }
 
-
+# Configuring password complexity in PAM, remembering last 4 passwords
 #ucredit=-1, the number of capital characters required in password
 #lcredit=-1, the number of lower case characters required in password
 #dcredit=-1, the number numbers required in password
 #ocredit=-1, the number of symbols required in password
-
+exec { "cp /etc/pam.d/system-auth /root;":
+  path   => '/usr/bin:/usr/sbin:/bin',
+}~>
 pam { "Set cracklib limits in password-auth":
   ensure    => present,
   service   => 'system-auth',
   type      => 'password',
   module    => 'pam_cracklib.so',
   arguments => ['try_first_pass','retry=3', 'minlen=10','ucredit=-1','lcredit=-1','dcredit=-1','ocredit=-1'],
-}
-
-
-pam { "Set password remember limit in password-auth":
+}~>
+pam { "Set password remember limit in system-auth":
   ensure    => present,
   service   => 'system-auth',
   type      => 'password',
   module    => 'pam_unix.so',
   arguments => ['md5','shadow', 'nullok','try_first_pass','use_authtok','remember=4'],
 }
-
 file { '/etc/security/opasswd':
   ensure => file,
   owner  => 'root',
@@ -62,22 +74,35 @@ file { '/etc/security/opasswd':
   mode   => '0600',
 }
 
+# Setting up SSHd options 
+exec { "cp /etc/ssh/sshd_config /root;":
+  path   => '/usr/bin:/usr/sbin:/bin',
+}~>
 class { 'ssh::server':
       storeconfigs_enabled => false,
       options => {
-        #'PasswordAuthentication' => 'no',
-        #'PermitRootLogin'        => 'no',
-        #'Port'                   => [22, 2222],
-        'Banner'	          => '/etc/issue.net',
+        'Banner'	                  => '/etc/issue.net',
+	'SyslogFacility'	          => 'AUTHPRIV',
+	'PermitRootLogin' 	          => 'yes',
+	'AuthorizedKeysFile'              => '.ssh/authorized_keys',
+	'PasswordAuthentication'          => 'no',
+	'ChallengeResponseAuthentication' => 'no',
+	'UsePAM' 			  => 'yes',
+	'X11Forwarding' 		  => 'yes',
+	'PrintLastLog' 			  => 'yes',
+	'AcceptEnv' 			  => 'LANG LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY LC_MESSAGES',
+	'AcceptEnv' 			  => 'LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT',
+	'AcceptEnv' 			  => 'LC_IDENTIFICATION LC_ALL LANGUAGE',
+	'AcceptEnv' 			  => 'XMODIFIERS',
+	'Subsystem' 			  => 'sftp  /usr/libexec/openssh/sftp-server',
       },
 }
 
-
+# Notify stanza for changes that require sshd to restart 
 class ssh {
 service { 'sshd':
     ensure  => 'running',
     enable  => true,
-    require => Package['openssh-server'],
   }
 
 # add a notify to the file resource
